b8a2f850a6a3afc7b08f702e4518d8e844126921
Baudouin <beyegbin-baudouin-venceslas.kouassi@imt-atlantique.net>
2018-10-18 01:57:22 +0200

    first commit

---
+++ TP_Intro/app/src/main/java/com/example/imt_atlantique/myfirstapplication/MainActivity.java
@@ -0,0 +1,46 @@
+package com.example.imt_atlantique.myfirstapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Log.i("Lifecycle", "onCreate method");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i("Lifecycle", "onDestroy method");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i("Lifecycle", "onRestart method");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i("Lifecycle", "onPause method");
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.i("Lifecycle", "onStart method");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i("Lifecycle", "onStop method");
+    }
+}
