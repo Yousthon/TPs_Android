c4b7985ccf154a9ba4a171c732e867b79dd63089
Baudouin <beyegbin-baudouin-venceslas.kouassi@imt-atlantique.net>
2018-10-18 03:57:05 +0200

    TP1

--- TP_1/app/src/main/java/com/example/imt_atlantique/myfirstapplication/MainActivity.java
+++ TP_1/app/src/main/java/com/example/imt_atlantique/myfirstapplication/MainActivity.java
@@ -1,8 +1,16 @@
 package com.example.imt_atlantique.myfirstapplication;
 
+import java.util.ArrayList;
+import java.util.List;
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.View;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.Spinner;
+import android.widget.ArrayAdapter;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,36 +19,17 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Spinner spinner = (Spinner) findViewById(R.id.SpinnerVilleNaiss);
+    // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.villeNaiss, android.R.layout.simple_spinner_item);
+// Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+// Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
+
         Log.i("Lifecycle", "onCreate method");
     }
 
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-        Log.i("Lifecycle", "onDestroy method");
-    }
 
-    @Override
-    protected void onRestart() {
-        super.onRestart();
-        Log.i("Lifecycle", "onRestart method");
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        Log.i("Lifecycle", "onPause method");
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        Log.i("Lifecycle", "onStart method");
-    }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        Log.i("Lifecycle", "onStop method");
-    }
 }
